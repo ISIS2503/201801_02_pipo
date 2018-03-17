576b01f9600c2bb7a819957a9958903ac5ded2e1
JUAN SEBASTIAN DIAZ SERRANO <js.diaz@sis.virtual.uniandes.edu.co>
2018-03-17 11:25:48 -0500

    borre todo pero sirve

--- entidadVirtual/RESTMock/app/controllers/Application.java
+++ entidadVirtual/RESTMock/app/controllers/Application.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import models.Ciudad;
 import play.*;
 import play.libs.Json;
 import play.mvc.*;
@@ -9,9 +8,5 @@ import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
-
-    public Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
-
+    
 }
