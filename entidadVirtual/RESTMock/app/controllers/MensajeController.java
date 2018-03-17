b60abc20326543f0459b1b0c443945e44511ac5d
SIS\s.guzmanm <supersergio.sgm@gmail.com>
2018-03-17 11:19:50 -0500

    Métodos para mirar el REST MOck

--- entidadVirtual/RESTMock/app/controllers/MensajeController.java
+++ entidadVirtual/RESTMock/app/controllers/MensajeController.java
@@ -2,19 +2,52 @@ package controllers;
 
 import com.avaje.ebean.Model;
 import com.fasterxml.jackson.databind.JsonNode;
-import models.Ciudad;
-import models.Mueble;
 import play.libs.Json;
 import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.Logger;
+import java.io.*;
 
 import java.util.List;
 
 /**
- * Created by scvalencia606 on 8/10/15.
+ * Created by s.guzmanm and js.diaz on 17/03/17.
  */
 public class MensajeController extends Controller {
+    BufferedWriter out;
+
+
+    public Result initializeBuffer()
+    {
+        if(out==null)
+        {
+            try
+            {
+                //out = new BufferedWriter(new OutputStreamWriter(System.out), 32768);
+                out = new BufferedWriter(new FileWriter(new File("./logs/prueba.txt")), 32768);
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        else
+        {
+            try
+            {
+                out.close();
+                out=null;
+            }
+            catch(Exception e)
+            {
+
+            }
+
+        }
+        return ok("");
+
+    }
     /**
      *
      * FORMATO JSON
@@ -37,17 +70,19 @@ public class MensajeController extends Controller {
      * }
      */
     @BodyParser.Of(BodyParser.Json.class)
-    public Result create() {
+    public Result create() throws Exception {
         JsonNode j = Controller.request().body().asJson();
         String mensaje="DE: "+j.findValue("remitente")+" PARA: ";
         for(JsonNode s:j.findValue("destinatarios"))
             mensaje+=s.findValue("correo")+";";
         mensaje+="ASUNTO: "+j.findValue("asunto")+";"+"CUERPO: "+j.findValue("cuerpo");
-        return ok(mensaje);
+        //System.out.println(mensaje);
+        out.write(mensaje+"\n");
+        //Logger.info(mensaje);
+        return ok("");
     }
 
     public Result read() {
-        List<Mueble> muebles = new Model.Finder(String.class, Mueble.class).all();
-        return ok(Json.toJson(muebles));
+        return ok("");
     }
 }
