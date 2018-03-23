a5ca9700caf43ca232ae4cead0715079f67dcd8c
SIS\s.guzmanm <supersergio.sgm@gmail.com>
2018-03-22 22:46:46 -0500

    Creación de un medido de latencia para P1 en Play

---
+++ entidadVirtual/P1LatencyTester/app/controllers/Application.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import play.*;
+import play.libs.Json;
+import play.mvc.*;
+
+import views.html.*;
+
+public class Application extends Controller {
+    
+}
