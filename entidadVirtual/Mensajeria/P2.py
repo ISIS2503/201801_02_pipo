e7b7b6c61b28df5df62b566bd9e01129c89a487e
Sergio Guzmán Mayorga <supersergio.sgm@gmail.com>
2018-04-11 22:03:46 -0500

    Actualización sobre la zona del programa P2

--- entidadVirtual/Mensajeria/P2.py
+++ entidadVirtual/Mensajeria/P2.py
@@ -2,10 +2,10 @@ import json
 import requests
 from kafka import KafkaConsumer
  
-topicos2=("Toscana.emergencia.aperturaSospechosa.2-503","Toscana.emergencia.puertaAbierta.2-503",
-         "Toscana.emergencia.aperturaNoPermitida.3-503","Toscana.emergencia.bateriaCritica.2-503")
+topicos2=("Centro.Toscana.emergencia.aperturaSospechosa.2-503","Centro.Toscana.emergencia.puertaAbierta.2-503",
+         "Centro.Toscana.emergencia.aperturaNoPermitida.3-503","Centro.Toscana.emergencia.bateriaCritica.2-503")
 
-consumer = KafkaConsumer(bootstrap_servers=['172.24.42.70:8090'])
+consumer = KafkaConsumer(bootstrap_servers=['172.24.42.64:8090'])
 consumer.subscribe(topicos2)
  
 for message in consumer:
@@ -16,6 +16,7 @@ for message in consumer:
 	unidad = emergency['conjunto']
 	inmuebleId = emergency['apartamento']
 	idEmergencia = emergency['emergencia']
+	zone=emergency['zona']
 	
 	tipoEmergencia = ''
 	if idEmergencia == str(1):
@@ -36,6 +37,7 @@ for message in consumer:
 		'fecha': sensetime,
 		'tipo': tipoEmergencia,
 		'idEmergencia' : idEmergencia
+		'zona':zone
     }
 	print(tipoEmergencia)
 	response = requests.post(url, data=json.dumps(payload),headers={'Content-type': 'application/json'})
