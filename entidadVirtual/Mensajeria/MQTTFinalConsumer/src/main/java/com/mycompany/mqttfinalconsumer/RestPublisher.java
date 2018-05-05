9f7f7d8fa8710f0c99cbf9406113427ff9c8a8c6
korkies22 <cm.sarmiento10@uniandes.edu.co>
2018-05-05 11:05:16 -0500

    Rest publisher ahora maneja errores

--- entidadVirtual/Mensajeria/MQTTFinalConsumer/src/main/java/com/mycompany/mqttfinalconsumer/RestPublisher.java
+++ entidadVirtual/Mensajeria/MQTTFinalConsumer/src/main/java/com/mycompany/mqttfinalconsumer/RestPublisher.java
@@ -15,22 +15,21 @@ import org.json.JSONObject;
  * @author s.guzmanm
  */
 public class RestPublisher extends Thread {
+
     private String url;
     private MqttMessage mqttMessage;
     private int contador;
-    private long time=System.currentTimeMillis();
+    private long time = System.currentTimeMillis();
+    public static int contadorErrores=0;
 
-    public RestPublisher(MqttMessage message,String url, int contador) {
+    public RestPublisher(MqttMessage message, String url, int contador) {
         this.mqttMessage = message;
-        this.url=url;
-        this.contador=contador;
+        this.url = url;
+        this.contador = contador;
     }
-    
-    
-    public void start()
-    {
-        try
-        {
+
+    public void start() {
+        try {
             //Base connection with RestAPI
             URL obj = new URL(url);
 
@@ -40,28 +39,29 @@ public class RestPublisher extends Thread {
             con.setRequestMethod("GET");
             con.setDoOutput(true);
 
-            JSONObject jsonRequest=new JSONObject(mqttMessage.toString()).getJSONObject("emergency");
-            System.out.println(SimpleMqqtConsumerClient.contador+" ASUNTO: Alarma del dispositivo "+ jsonRequest.getString("id")+";CONTENIDO: Hubo una emergencia en el inmueble "+jsonRequest.getString("apartamento")+
-                    " de la unidad residencial "+jsonRequest.getString("conjunto")+" de la zona "+jsonRequest.getString("zona")+" de tipo "+jsonRequest.getString("emergencia")+";REMITENTE:s.guzmanm@yale.com"
-                    +";DESTINATARIOS:se.cardenas@uniandes.edu.co,ja.manrique@uniandes.edu.co");
-            if(con.getResponseCode()!=200)
-                throw new Exception("Falló P1 ");
-            SimpleMqqtConsumerClient.sumatoria[contador]=System.currentTimeMillis()-time;
-            if(contador>=999999)
-            {
-                long acumulado=0;
-                for (long num: SimpleMqqtConsumerClient.sumatoria) {
-                    acumulado+=num;                  
-                }
-                 System.out.println(acumulado/100000);
+            JSONObject jsonRequest = new JSONObject(mqttMessage.toString()).getJSONObject("emergency");
+            System.out.println(SimpleMqqtConsumerClient.contador + " ASUNTO: Alarma del dispositivo " + jsonRequest.getString("id") + ";CONTENIDO: Hubo una emergencia en el inmueble " + jsonRequest.getString("apartamento")
+                    + " de la unidad residencial " + jsonRequest.getString("conjunto") + " de la zona " + jsonRequest.getString("zona") + " de tipo " + jsonRequest.getString("emergencia") + ";REMITENTE:s.guzmanm@yale.com"
+                    + ";DESTINATARIOS:se.cardenas@uniandes.edu.co,ja.manrique@uniandes.edu.co");
+            if (con.getResponseCode() != 200) {
+                contadorErrores++;
+
             }
-        }
-        catch(Exception e)
-        {
+            SimpleMqqtConsumerClient.sumatoria[contador] = System.currentTimeMillis() - time;
+            if (contador >= 999999) {
+                long acumulado = 0;
+                for (long num : SimpleMqqtConsumerClient.sumatoria) {
+                    acumulado += num;
+                }
+                double resp= (acumulado / 100000);
+                System.out.println("promedio "+ resp);
+                System.out.println("Contador errores "+  contadorErrores);
+            }
+        } catch (Exception e) {
             System.out.println(SimpleMqqtConsumerClient.contador);
             e.printStackTrace();
         }
-        
+
     }
-    
+
 }
