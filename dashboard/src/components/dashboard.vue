6fba15710224d8d9ea9f5ca2e1d8dd6219c9bac6
KORKIES22\CarlosMario <cm.sarmiento10@uniandes.edu.co>
2018-05-12 21:08:53 -0500

    Arreglado bootstrap

--- dashboard/src/components/dashboard.vue
+++ dashboard/src/components/dashboard.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="dashboard">
-    Hello dashboard
+   <b-card no-block>
+    <b-tabs>
+      <b-tab title="Tab 1">
+        Tab 1 Contents
+      </b-tab>
+      <b-tab title="Tab 2">
+        Tab 2 Contents <b-button size="md">Boop</b-button>
+      </b-tab>
+      <b-tab title="Tab 3" disabled>
+        Tab 3 Contents
+      </b-tab>
+    </b-tabs>
+  </b-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'dashboard',
-  data () {
+  name: "dashboard",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to Your Vue.js App"
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- dashboard/src/router/index.js
+++ dashboard/src/router/index.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
 import Router from 'vue-router'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-vue/dist/bootstrap-vue.css"
 import HelloWorld from '@/components/HelloWorld'
 import Dashboard from '@/components/Dashboard'
 
