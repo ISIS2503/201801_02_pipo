1dc834d1acae260a5425e38053462d049c868737
JULIAN ALBERTO MANRIQUE PUERTO <ja.manrique@sis.virtual.uniandes.edu.co>
2018-05-13 15:51:22 -0500

    Ahora P2 recibe fallos y los persiste en flask

--- entidadVirtual/Mensajeria/P2.py
+++ entidadVirtual/Mensajeria/P2.py
@@ -5,35 +5,16 @@ from bson.json_util import dumps, loads, ObjectId, CANONICAL_JSON_OPTIONS
  
 topicos2=("Centro.Toscana.emergencia.aperturaSospechosa.2-5-3","Centro.Toscana.emergencia.puertaAbierta.2-5-3",
          "Centro.Toscana.emergencia.aperturaNoPermitida.2-5-3","Centro.Toscana.emergencia.bateriaCritica.2-5-3")
-
+topicosFallos=("Centro.Toscana.fallo.cerraduraFueraDeLinea.2-5-3","Centro.Toscana.fallo.hubFueraDeLinea.2-5-3")
 consumer = KafkaConsumer(bootstrap_servers=['172.24.42.70:8090'])
 consumer.subscribe(topicos2)
+consumer.subscribe(topicosFallos)
 
 isAuthenticated=False
 print(topicos2)
+print(topicosFallos)
 print(consumer)
 for message in consumer:
-	print(message)
-	location = message.topic.split('.')
-	sensor_code = location[1] + '.' + location[2]
-	
-	json_data = json.loads(message.value.decode('utf-8'))
-	sensetime = json_data['sensetime']
-	emergency = json_data['emergency']
-	
-	unidad = emergency['conjunto']
-	inmuebleId = emergency['apartamento']
-	idEmergencia = emergency['emergencia']
-	
-	tipoEmergencia = ''
-	if idEmergencia == str(1):
-		tipoEmergencia = 'Puerta abierta'
-	elif idEmergencia == str(2):
-		tipoEmergencia = 'Apertura sospechosa'
-	elif idEmergencia == str(3):
-		tipoEmergencia = 'Apertura no permitida'
-	elif idEmergencia == str(4):
-		tipoEmergencia = 'Bateria baja'	
 	if isAuthenticated==False:
 		url='https://isis2503-jamanrique.auth0.com/oauth/token'
 		payload={
@@ -60,17 +41,64 @@ for message in consumer:
 					}
 		print("SESION",type(sessionParam))
 		isAuthenticated=True	
-	url = 'http://172.24.42.64/p2/unidadesResidenciales/' + unidad + '/inmuebles/' + inmuebleId + '/hub/cerradura/emergencias'
-	payload = {
-		'fecha': sensetime,
-		'tipo': tipoEmergencia,
-		'idEmergencia' : idEmergencia
-    }
-	print(tipoEmergencia)
+
+	print(message)
+	if message.topic in topicos2:
+		location = message.topic.split('.')
+		sensor_code = location[1] + '.' + location[2]
+		
+		json_data = json.loads(message.value.decode('utf-8'))
+		sensetime = json_data['sensetime']
+		emergency = json_data['emergency']
+		
+		unidad = emergency['conjunto']
+		inmuebleId = emergency['apartamento']
+		idEmergencia = emergency['emergencia']
+		
+		tipoEmergencia = ''
+		if idEmergencia == str(1):
+			tipoEmergencia = 'Puerta abierta'
+		elif idEmergencia == str(2):
+			tipoEmergencia = 'Apertura sospechosa'
+		elif idEmergencia == str(3):
+			tipoEmergencia = 'Apertura no permitida'
+		elif idEmergencia == str(4):
+			tipoEmergencia = 'Bateria baja'	
+		
+		url = 'http://172.24.42.64/p2/unidadesResidenciales/' + unidad + '/inmuebles/' + inmuebleId + '/hub/cerradura/emergencias'
+		payload = {
+			'fecha': sensetime,
+			'tipo': tipoEmergencia,
+			'idEmergencia' : idEmergencia
+	    }
+	elif message.topic in topicosFallos:
+		location = message.topic.split('.')
+		sensor_code = location[1] + '.' + location[2]
+		
+		json_data = json.loads(message.value.decode('utf-8'))
+		sensetime = json_data['sensetime']
+		failure = json_data['failure']
+		
+		unidad = failure['conjunto']
+		inmuebleId = failure['apartamento']
+		idFallo = failure['fallo']
+		
+		tipoFallo = ''
+		if idFallo == str(1):
+			tipoFallo = 'Cerradura fuera de linea'
+		elif idFallo == str(2):
+			tipoFallo = 'Hub fuera de linea'
+		
+		url = 'http://172.24.42.64/p2/unidadesResidenciales/' + unidad + '/inmuebles/' + inmuebleId + '/hub/fallos'
+		payload = {
+			'fecha': sensetime,
+			'tipo': tipoFallo,
+			'idFallo' : idFallo
+	    }
 	response = requests.post(url, data=json.dumps(payload),headers={'Content-type': 'application/json','sessionParam':json.dumps(sessionParam)})
 	print("Response Status Code: " + str(response.status_code))
 	print(response.content)
-	if(response.status_code==403):
+	if(response.status_code!=200):
 		isAuthenticated=False
 	
 	
