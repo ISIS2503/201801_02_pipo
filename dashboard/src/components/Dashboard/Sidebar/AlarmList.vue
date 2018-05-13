18df7e8d13ae15102006a96aee5788e86942af69
Julián Manrique <ja.manrique@uniandes.edu.co>
2018-05-13 00:24:09 -0500

    Alarm, Alarmlist and Detail updates

--- dashboard/src/components/Dashboard/Sidebar/AlarmList.vue
+++ dashboard/src/components/Dashboard/Sidebar/AlarmList.vue
@@ -1,11 +1,34 @@
 <template>
-  
+  <div class="md-scrollbar">
+    <alarm 
+      v-for="alarm, index in filteredAlarms"
+      :key="alarm.timestamp"
+      tower="alarm.torre"
+      apartment="alarm.apartment"
+      alarm="alarm.alarm"
+    />
+  </div>
 </template>
 
 <script>
+import Alarm from './Alarm'
 export default {
-  name: 'AlarmList'
-}
+  name: "AlarmList",
+  components:{
+    Alarm
+  },
+  props: ["alarms"],
+  data() {
+    return {
+      filters: []
+    }
+  },
+  computed:{
+    filteredAlarms(){
+      return alarms
+    }
+  }
+};
 </script>
 
 <style scoped>
--- dashboard/src/components/Dashboard/Sidebar/Detail.vue
+++ dashboard/src/components/Dashboard/Sidebar/Detail.vue
@@ -1,10 +1,37 @@
 <template>
-  
+  <div>
+    <div>
+    <h1>
+      Torre {{tower}} - Apartamento {{apartment}}
+    </h1>
+    <h2 class="error" v-if="error">{{parseError}}</h2>
+    </div>
+    <div>
+      <h2>Propietario</h2>
+      <p>{{owner.nombre}}</p>
+      <p>{{owner.phone}}</p>
+      <p v-if="owner.email">{{owner.email}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Detail'
+  name: 'Detail',
+  props: ['tower', 'apartment', 'error'],
+  data(){
+    return{
+      owner: null
+    }
+  },
+  mounted(){
+    //retrieve owner data
+  },
+  computed:{
+    parseError(){
+      return error
+    }
+  }
 }
 </script>
 
