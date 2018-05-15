7820dfd06b8246786cd6c84e79724356014e6ba6
Julián Manrique <ja.manrique@uniandes.edu.co>
2018-05-14 23:06:32 -0500

    Dashboard websocket init

--- dashboard/src/components/Dashboard/Dashboard.vue
+++ dashboard/src/components/Dashboard/Dashboard.vue
@@ -2,10 +2,10 @@
 <div class="dashboard">
     <div class="md-layout">
         <div class="md-layout-item md-size-75">
-            <grids></grids>
+            <grids/>
         </div>
         <div class="md-layout-item md-size-25 sidebar-container">
-            <sidebar class="sidebar"></sidebar>
+            <sidebar class="sidebar"/>
         </div>
     </div>
 </div>
@@ -14,19 +14,56 @@
 
 
 <script>
-import Grids from './Grids/Grids.vue'
-import Sidebar from './Sidebar/Sidebar.vue'
+import Grids from "./Grids/Grids.vue";
+import Sidebar from "./Sidebar/Sidebar.vue";
 export default {
-    name: "dashboard",
-    components: {
-        Grids,
-        Sidebar
+  name: "dashboard",
+  components: {
+    Grids,
+    Sidebar
+  },
+  data() {
+    return {
+      websocketConnected: false,
+      //Contains incoming alarms & failures
+      emergencies: []
+    };
+  },
+  methods: {
+    //Initializes SocketIO and declares event listener for emergencies
+    initWebsocket() {
+      const serverIP = "http://172.24.42.64";
+      const namespace = "/securityWebsocket";
+      //Conectarse al servidor
+      let socket = io.connect(serverIP + namespace);
+
+      const _this = this;
+      // Event handler for server receive data.
+      socket.on("emergency", msg => {
+        if (_this.websocketConnected) {
+          //Convert to js object
+          const emergency = JSON.parse(msg);
+          console.log(emergency);
+          _this.emergencies.push(data);
+        } else {
+          if (msg == "Connected") _this.websocketConnected = true;
+          else
+            console.log(
+              "El Websocket aún no ha sido inicializado, se esperaba 'Connected' se recibió: ",
+              msg
+            );
+        }
+      });
     },
-    data() {
-        return {
-            msg: "Welcome to Your Vue.js App"
-        };
+    //Retireves information from server and parses it to fit front-end structure
+    initData(){
+
     }
+  },
+  mounted() {
+    this.initWebsocket()
+    this.initData()
+  }
 };
 </script>
 
@@ -34,9 +71,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-
-.sidebar-container{
+.sidebar-container {
   position: fixed;
   right: 15px;
 }
